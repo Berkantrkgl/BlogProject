36d4547cb19a2e8431a55a7477bf66e28346f9f0
Berkan Turkoglu <berkan.trkgl35@gmail.com>
2023-11-30 13:37:46 +0300

    Add write event and edit posts section

--- app.js
+++ app.js
@@ -5,8 +5,40 @@ const app = express();
 const port = 3011;
 
 app.use(bodyParser.urlencoded({ extended: true}));
-
 app.use(express.static("public"))
+
+const result = [];
+var length;
+
+// For pushing the data to a global result array
+function addPosts(data) {
+    length = result.length;
+    result.push(data);
+}
+
+// Get submitted data in particular form
+function getData(req) { 
+    var minute = "0";
+    var contentLength = req.body["content"].length;
+    if ( contentLength < 200) {
+        minute = "1";
+    } else if ( contentLength < 400 && contentLength > 200) {
+        minute = "3";
+    } else if (contentLength < 700 && contentLength > 400) {
+        minute = "6";
+    } else {
+        minute = "10";
+    }   
+    const line = req.body["content"].slice(0,100);
+    const data = {
+        userName: req.body["name"],
+        title: req.body["title"],
+        content: req.body["content"],
+        oneline: line,
+        min: minute,
+    }
+    return data;
+}
 
 // For the home page
 app.get("/", (req, res) => {
@@ -20,7 +52,10 @@ app.get("/home", (req, res) => {
 
 // To click to posts section of the header
 app.get("/posts", (req, res) => {
-    res.render("posts.ejs");
+    res.render("posts.ejs", {
+        array: result,
+        index: length,
+    });
 });
 
 app.get("/write", (req, res) => {
@@ -32,10 +67,20 @@ app.get("/about", (req, res) => {
 })
 
 app.post("/submit", (req, res) => {
-    console.log(req.body);
+    const data = getData(req);
+    addPosts(data);
     res.render("posts.ejs", {
-        
-    })
+        array: result,
+        index: length,
+    });
+})
+
+app.get("/details", (req, res) => {
+    console.log(req.body)
+    res.render("postDetail.ejs", {
+        array: result,
+        index: length,
+    });
 })
 
 app.listen(port, () => {
